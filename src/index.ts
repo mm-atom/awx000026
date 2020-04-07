c3b0a62bf4788519bdccccacf6fcd387489ff267
taoqf <tao_qiufeng@126.com>
2020-04-07 10:48:03 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-export default function fun() {
+
+interface IAi {
+	readonly data: {
+		readonly [attr: string]: unknown;
+	};
+	fire(action: string, ...args: unknown[]): Promise<unknown>;
+	emit(event: string, ...args: unknown[]): Promise<unknown>;
+}
+
+interface IAiWeappAppData {
+	readonly [attr: string]: unknown;
+	readonly app: wx.App;
+	inner_audio_context?: wx.InnerAudioContext;
+	background_audio_manager?: wx.BackgroundAudioManager;
+	record_manager?: wx.RecorderManager;
+	video_context?: wx.VideoContext;
+	camera_context?: wx.CameraContext;
+}
+
+export default interface IAiWeappApp extends IAi {
+	readonly data: IAiWeappAppData;
+	// eslint-disable-next-line semi
 }
